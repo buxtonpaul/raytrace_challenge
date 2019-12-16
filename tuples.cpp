474acfe741f20ab93fc8c3bdb654c7fef73193f0
Paul Buxton <paulbuxton.mail@googlemail.com>
2019-12-16 16:57:23 +0000

    Move some of the functinos in to the cpp file

--- tuples.cpp
+++ tuples.cpp
@@ -1,1 +1,24 @@
 #include "tuples.h"
+
+bool comparator(const double & left, const double & right){
+                    // Lambda function to compare 2 strings in case insensitive manner
+                    return  fabs(left - right) < EPSILON;
+                  }
+
+bool arraycomparator(const std::array<double, 4> & lhs, const std::array<double, 4> & rhs){
+  return std::equal(lhs.begin(), lhs.end(), rhs.begin(), rhs.end(), comparator);
+}
+
+bool   operator ==( const Raytuple &lhs, const Raytuple &rhs)   {return arraycomparator(lhs.Values(),  rhs.Values()); }
+bool  operator !=( const Raytuple &lhs, const Raytuple &rhs)   {return !(arraycomparator(lhs.Values(),  rhs.Values())); }
+
+Raytuple Vector(double x, double y, double z) {return Raytuple(x, y, z, 0.0);}
+Raytuple Point(double x, double y, double z) {return Raytuple(x, y, z, 1.0);}
+
+Raytuple  operator +(const Raytuple &lhs, const Raytuple &rhs) { return Raytuple::arrayadder(lhs, rhs);}
+Raytuple  operator -(const Raytuple &lhs, const Raytuple &rhs) { return Raytuple::arraysub(lhs, rhs);}
+
+Raytuple  operator *(const double &lhs, const Raytuple &rhs) { return Raytuple::arraymul(lhs, rhs);}
+Raytuple  operator *(const Raytuple  &lhs, const double &rhs) { return Raytuple::arraymul(rhs, lhs);}
+Raytuple  operator /(const Raytuple  &lhs, const double &rhs) { return Raytuple::arraymul(1.0 / rhs, lhs);}
+Raytuple  operator -(const Raytuple &rhs) { return (Raytuple(0, 0, 0, 0) - rhs);}
--- tuples.h
+++ tuples.h
@@ -9,14 +9,10 @@ static double EPSILON = std::numeric_limits<double>::epsilon();
 // these are represented using 4 elements x,y,z,w
 // where w==0 means it is a vector and w==1 means it is a point
 
-bool comparator(const double & left, const double & right){
-                    // Lambda function to compare 2 strings in case insensitive manner
-                    return  fabs(left - right) < EPSILON;
-                  }
+// non class helpers
+bool comparator(const double & left, const double & right);
 
-bool arraycomparator(const std::array<double, 4> & lhs, const std::array<double, 4> & rhs){
-  return std::equal(lhs.begin(), lhs.end(), rhs.begin(), rhs.end(), comparator);
-}
+bool arraycomparator(const std::array<double, 4> & lhs, const std::array<double, 4> & rhs);
 
 class Raytuple{
   public:
@@ -64,18 +60,20 @@ class Raytuple{
   const std::array <double, 4> _vals;
 };
 
-bool   operator ==( const Raytuple &lhs, const Raytuple &rhs)   {return arraycomparator(lhs.Values(),  rhs.Values()); }
-bool  operator !=( const Raytuple &lhs, const Raytuple &rhs)   {return !(arraycomparator(lhs.Values(),  rhs.Values())); }
 
-Raytuple Vector(double x, double y, double z) {return Raytuple(x, y, z, 0.0);}
-Raytuple Point(double x, double y, double z) {return Raytuple(x, y, z, 1.0);}
+bool   operator ==(const Raytuple &lhs, const Raytuple &rhs);
+bool  operator !=(const Raytuple &lhs, const Raytuple &rhs);
 
-Raytuple  operator +(const Raytuple &lhs, const Raytuple &rhs) { return Raytuple::arrayadder(lhs, rhs);}
-Raytuple  operator -(const Raytuple &lhs, const Raytuple &rhs) { return Raytuple::arraysub(lhs, rhs);}
+Raytuple Vector(double x, double y, double z);
+Raytuple Point(double x, double y, double z);
 
-Raytuple  operator *(const double &lhs, const Raytuple &rhs) { return Raytuple::arraymul(lhs, rhs);}
-Raytuple  operator *(const Raytuple  &lhs, const double &rhs) { return Raytuple::arraymul(rhs, lhs);}
-Raytuple  operator /(const Raytuple  &lhs, const double &rhs) { return Raytuple::arraymul(1.0 / rhs, lhs);}
-Raytuple  operator -(const Raytuple &rhs) { return (Raytuple(0, 0, 0, 0) - rhs);}
+// Operators
+Raytuple  operator +(const Raytuple &lhs, const Raytuple &rhs);
+Raytuple  operator -(const Raytuple &lhs, const Raytuple &rhs);
+
+Raytuple  operator *(const double &lhs, const Raytuple &rhs);
+Raytuple  operator *(const Raytuple  &lhs, const double &rhs);
+Raytuple  operator /(const Raytuple  &lhs, const double &rhs);
+Raytuple  operator -(const Raytuple &rhs);
 
 
