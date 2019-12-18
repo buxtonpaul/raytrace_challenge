39ad968dab560e4f17b52c22a338f370d2c14b84
Paul Buxton <paulbuxton.mail@googlemail.com>
2019-12-18 19:01:50 +0000

    Improve the float comparison to be based on the range of the numbers

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,14 +1,14 @@
 #include "utils.h"
 #include <cmath>
 #include <algorithm>
-
+#include <iostream>
 #define min(a, b)((a) > (b) ? (b) :(a))
 
 bool float_equals(const double &lhs, const double &rhs)
 {
-  // double margin = min(fabs(lhs), fabs(rhs)) * 0.001;
+   double margin = (min(fabs(lhs), fabs(rhs))) * 0.001;
   // set the margin to be .1 % of the smallest number
-  if (fabs(lhs - rhs) <0.001)
+  if (fabs(lhs - rhs) <= margin)
     return true;
   return  false;
 }
