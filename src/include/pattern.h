9931cf177ef88aefdc7c4fee67a8dfc3f2d5a917
Paul Buxton <paulbuxton.mail@googlemail.com>
2021-03-27 15:04:41 +0000

    Update const and switch to references.

--- src/include/pattern.h
+++ src/include/pattern.h
@@ -6,52 +6,52 @@
 
 namespace ray_lib
 {
-class Pattern
-{
-public:
-  virtual Color getColor(const Point p, const Matrix  &worldtransform = Matrix::Identity) const = 0;
-  Pattern *asPattern() { return reinterpret_cast<Pattern *>(this); }
-};
-
-class StripePattern : public Pattern
-{
-public:
-  Color getColor(const Point p, const Matrix &worldtransform  = Matrix::Identity) const;
-  Color getColor_a() const { return _a; }
-  Color getColor_b() const { return _b; }
-  StripePattern(Color a, Color b, Matrix transform = Matrix::Identity) : _a(a), _b(b), _transform(transform) {}
-  Color setColor_a(const Color &newcol)
+  class Pattern
   {
-    _a = newcol;
-    return _a;
-  }
-  Color setColor_b(const Color &newcol)
+  public:
+    virtual Color getColor(const Point &p, const Matrix &worldtransform = Matrix::Identity) const = 0;
+    Pattern *asPattern() { return reinterpret_cast<Pattern *>(this); }
+  };
+
+  class StripePattern : public Pattern
   {
-    _b = newcol;
-    return _b;
-  }
+  public:
+    Color getColor(const Point &p, const Matrix &worldtransform = Matrix::Identity) const;
+    Color getColor_a() const { return _a; }
+    Color getColor_b() const { return _b; }
+    StripePattern(const Color &a, const Color &b, const Matrix &transform = Matrix::Identity) : _a(a), _b(b), _transform(transform) {}
+    Color setColor_a(const Color &newcol)
+    {
+      _a = newcol;
+      return _a;
+    }
+    Color setColor_b(const Color &newcol)
+    {
+      _b = newcol;
+      return _b;
+    }
 
-private:
-  Color _a;
-  Color _b;
-  Matrix _transform;
-};
+  private:
+    Color _a;
+    Color _b;
+    Matrix _transform;
+  };
 
-class SolidPattern : public Pattern
-{
-  Color _a;
-
-public:
-  Color getColor(const Point p, const Matrix  &worldtransform  = Matrix::Identity) const;
-  explicit SolidPattern(Color a = Color::White) : _a(a) {}
-  Color setColor(const Color &newcol)
+  class SolidPattern : public Pattern
   {
-    _a = newcol;
-    return _a;
-  }
-};
+    Color _a;
 
-extern SolidPattern SolidWhite;
+  public:
+    Color getColor(const Point &p, const Matrix &worldtransform = Matrix::Identity) const;
+    explicit SolidPattern(const Color &a = Color::White) : _a(a) {}
+    Color setColor(const Color &newcol)
+    {
+      _a = newcol;
+      return _a;
+    }
+  };
+
+  extern SolidPattern SolidWhite;
 
 } // namespace ray_lib
 #endif // _pattern_h_
--- src/modules/pattern.cpp
+++ src/modules/pattern.cpp
@@ -3,13 +3,13 @@
 #include "tuples.h"
 namespace ray_lib
 {
-  Color StripePattern::getColor(ray_lib::Point p, const Matrix &worldtransform) const
+  Color StripePattern::getColor(const ray_lib::Point &p, const Matrix &worldtransform) const
   {
     int c{static_cast<int>(floor(p.x()))};
     return ((c % 2) == 0 ? _a : _b);
   }
 
-  Color SolidPattern::getColor(ray_lib::Point p, const Matrix &worldtransform) const
+  Color SolidPattern::getColor(const ray_lib::Point &p, const Matrix &worldtransform) const
   {
     return (_a);
   }
